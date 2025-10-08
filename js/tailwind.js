11224536bc912ead59b3f8e196e84e7c9e8768e5
Pablo Wynistorf <mail@pablo.one>
2025-10-08 22:25:00 +0200

    remove tailwind config from html

---
+++ js/tailwind.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                display: ['"Playfair Display"', 'serif'],
+                sans: ['"Source Sans 3"', 'sans-serif']
+            },
+            colors: {
+                brand: {
+                    50: '#f6f5f2',
+                    100: '#ede9df',
+                    200: '#d8cfb6',
+                    300: '#c2b38d',
+                    400: '#ac9663',
+                    500: '#967b3d',
+                    600: '#7a6130',
+                    700: '#5d4824',
+                    800: '#413018',
+                    900: '#241a0c'
+                }
+            }
+        }
+    }
+}
